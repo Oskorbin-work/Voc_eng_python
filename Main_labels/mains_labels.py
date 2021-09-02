394dee26580a7969377ca8d6edf77600570e14e1
Oskorbin <serhiy.polozyuk@m.hneu.edu.ua>
2021-09-02 16:44:44 +0300

    Refractor code day 2 work with data base

--- Main_labels/mains_labels.py
+++ Main_labels/mains_labels.py
@@ -1,11 +1,18 @@
-from PyQt5.QtCore import QTimer
+# -----------------------------------------------------------
+# Import classical and Pyqt5`s modules
+# -----------------------------------------------------------
 from PyQt5.QtGui import QFont
-from PyQt5.uic.properties import QtCore
 from PyQt5.QtWidgets import QLabel
 from PyQt5 import QtCore
 
+import os # Initiate project into operating system
+import sqlite3 # Database to Tooltip
 
+from setting import ROOT_DIR
+
+# Class to describe  main labels
 class Main_labels:
+    # create lables
     def name_labels(self):
         self.time = QtCore.QTime(0, 0, 0)
         self.timer_learn = QLabel('Время: 00:00:00')
@@ -20,43 +27,76 @@ class Main_labels:
         self.transcription_word_now = QLabel('Транскрипция: ')
         self.chapter_word_now = QLabel('Тема: ')
 
+    # describe font and size labels text
     def label_set_font_and_size(self):
         size_label_font = 10
         name_label_font = 'Arial'
-        self.timer_learn.setFont(QFont(name_label_font, size_label_font))
-        self.sum_words_learn.setFont(QFont(name_label_font, size_label_font))
-        self.false_words.setFont(QFont(name_label_font, size_label_font))
-        self.true_words.setFont(QFont(name_label_font, size_label_font))
-        self.change_words_learn.setFont(QFont(name_label_font, size_label_font))
-        self.count_word_now.setFont(QFont(name_label_font, size_label_font))
-        self.parts_of_speech_word_now.setFont(QFont(name_label_font, size_label_font))
-        self.word_now.setFont(QFont(name_label_font, size_label_font))
-        self.transcription_word_now.setFont(QFont(name_label_font, size_label_font))
-        self.chapter_word_now.setFont(QFont(name_label_font, size_label_font))
+        self.timer_learn.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.sum_words_learn.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.false_words.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.true_words.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.change_words_learn.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.count_word_now.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.parts_of_speech_word_now.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.word_now.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.transcription_word_now.\
+            setFont(QFont(name_label_font, size_label_font))
+        self.chapter_word_now.\
+            setFont(QFont(name_label_font, size_label_font))
 
+    # change word
     def label_set_text(self):
-        self.sum_words_learn.setText(self.sum_words_learn.text() +"d")
-        self.false_words.setText(self.false_words.text() +"d")
-        self.true_words.setText(self.true_words.text() +"d")
-        self.change_words_learn.setText(self.change_words_learn.text() +"d")
-        self.count_word_now.setText(self.count_word_now.text() +"d")
-        self.parts_of_speech_word_now.setText(self.parts_of_speech_word_now.text() +"d")
-        self.word_now.setText(self.word_now.text() +"d")
-        self.transcription_word_now.setText(self.transcription_word_now.text() +"d")
-        self.chapter_word_now.setText(self.chapter_word_now.text() +"d")
+        random_word = 1
+        # Connect database
+        conn = sqlite3.connect(ROOT_DIR + "database" + '\\' + 'data_word.sqlite')
+        cur = conn.cursor()
+        # get all command from database
+        cur.execute("select * from Main_table where id_main  = %d;"% random_word)
+        conn.commit()
+        ones = (cur.fetchall())[0]
+        print(ones)
+        self.sum_words_learn.\
+            setText(self.sum_words_learn.text() + ones[1])
+        self.false_words.\
+            setText(self.false_words.text() + ones[1])
+        self.true_words.\
+            setText(self.true_words.text() + ones[1])
+        self.change_words_learn.\
+            setText(self.change_words_learn.text() + ones[1])
+        self.count_word_now.\
+            setText(self.count_word_now.text() + str(ones[6]))
+        self.parts_of_speech_word_now.\
+            setText(self.parts_of_speech_word_now.text() + ones[1])
+        self.word_now.\
+            setText(self.word_now.text() + ones[3])
+        self.transcription_word_now.\
+            setText(self.transcription_word_now.text() + str(ones[4]))
+        self.chapter_word_now.\
+            setText(self.chapter_word_now.text() + ones[7])
 
+
+    # create timer
     def timer(self):
-        self.timer_learn_1 = QTimer(self)
+        self.timer_learn_1 = QtCore.QTimer(self)
         self.timer_learn_1.setInterval(1000)
         self.timer_learn_1.timeout.connect(self.timer_text)
         self.timer_learn_1.start()
 
+    # change time to timer
     def timer_text(self):
         self.time = self.time.addSecs(1)
         self.timer_learn.setText(self.time.toString("Время: hh:mm:ss"))
 
+    # main label def
     def main_label_def(self):
         self.name_labels()
-        # initiate functions
         self.label_set_font_and_size()
         self.label_set_text()
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 # -----------------------------------------------------------
 # Codes other files project
 # -----------------------------------------------------------
-from Main_labels.mains_labels import Main_labels  # Initiate Labels from main window
+# Initiate Labels from main window
+from Main_labels.mains_labels import Main_labels
 from GUI.bar import Bar  # Initiate bar-structure
 # -----------------------------------------------------------
 # Import classical and Pyqt5`s modules
 # -----------------------------------------------------------
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame, QGridLayout, QWidget
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QGridLayout, QWidget,
+)
 
 
 # Class to describe structure main window
@@ -37,19 +40,36 @@ class Main_windows(QMainWindow, Bar, Main_labels):
         # create Grid
         grid = QGridLayout()
 
+        # -----------------------------------------------------------
         # add main labels
         # Information about words
-        grid.addWidget(self.timer_learn, 1, 1)  # timer from start
-        grid.addWidget(self.sum_words_learn, 2, 0)  # count words are for learning
-        grid.addWidget(self.false_words, 3, 0)  # count words to without verification
-        grid.addWidget(self.true_words, 4, 0)  # count words to with verification
-        grid.addWidget(self.change_words_learn, 5, 0)  # count words that can change
+        # -----------------------------------------------------------
+
+        # timer from start
+        grid.addWidget(self.timer_learn, 1, 1)
+        # count words are for learning
+        grid.addWidget(self.sum_words_learn, 2, 0)
+        # count words to without verification
+        grid.addWidget(self.false_words, 3, 0)
+        # count words to with verification
+        grid.addWidget(self.true_words, 4, 0)
+        # count words that can change
+        grid.addWidget(self.change_words_learn, 5, 0)
+
+        # -----------------------------------------------------------
         # Word that is checking
-        grid.addWidget(self.word_now, 6, 0)  # Word is checking.
-        grid.addWidget(self.count_word_now, 7, 0)  # count words are for learning
-        grid.addWidget(self.parts_of_speech_word_now, 8, 0)  # Part of speech
-        grid.addWidget(self.transcription_word_now, 9, 0)  # Transcription
-        grid.addWidget(self.chapter_word_now, 10, 0) # Chapter
+        # -----------------------------------------------------------
+
+        # Word is checking.
+        grid.addWidget(self.word_now, 6, 0)
+        # count words are for learning
+        grid.addWidget(self.count_word_now, 7, 0)
+        # Part of speech
+        grid.addWidget(self.parts_of_speech_word_now, 8, 0)
+        # Transcription
+        grid.addWidget(self.transcription_word_now, 9, 0)
+        # Chapter
+        grid.addWidget(self.chapter_word_now, 10, 0)
 
         # This is widget!
         widget = QWidget()
@@ -57,6 +77,7 @@ class Main_windows(QMainWindow, Bar, Main_labels):
         self.setCentralWidget(widget)
 
 
+# It for run program
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Main_windows()
